ee56fe1f7584a7058f9ba1dceedab23099eb0b77
Jeffrey Biles <jeffreybiles@Jeffreys-MacBook-Pro-2.local>
2014-04-10 23:52:30 -0500

    create and show methods

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,4 +5,20 @@ class Api::V1::PostsController < ApplicationController
     @posts = Post.all
     render json: @posts
   end
+
+  def create
+    @post = Post.create(params[:post])
+    render json: @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+    render json: @post
+  end
 end
